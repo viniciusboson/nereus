8ab188fb6edd645d7e5639ff7d36142254b06d64
Vinicius Kairala <vkairala@squaretrade.com>
2017-10-30 13:29:53 -0200

    WIP changes to automatically match Operation/Position/Transaction

--- src/main/webapp/app/entities/operation/operation.model.ts
+++ src/main/webapp/app/entities/operation/operation.model.ts
@@ -1,11 +1,20 @@
 import { BaseEntity } from './../../shared';
 
+export const enum OperationType {
+    'WIRE_TRANSFER',
+    'WITHDRAW',
+    'DEPOSIT',
+    'BUY',
+    'SELL'
+}
+
 export class Operation implements BaseEntity {
     constructor(
         public id?: number,
         public executedAt?: any,
         public amountFrom?: number,
         public amountTo?: number,
+        public operationType?: OperationType,
         public positionFromId?: number,
         public institutionFromId?: number,
         public positionToId?: number,
